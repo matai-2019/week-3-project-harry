113ec1887e0c648eb0a8efe65e935cad3a671f4d
Taine Walford <tainewalford@live.com>
2019-06-14 10:04:56 +1200

    added packages and dbReset script

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
-const userRoutes = require('./routes/users')
+const beastRoutes = require('./routes/users')
 
 const server = express()
 
 // Middleware
 
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
-server.use(express.urlencoded({extended: true}))
+server.use(express.urlencoded({ extended: true }))
 
 // Routes
 
-server.use('/', userRoutes)
+server.use('/', beastRoutes)
 
 module.exports = server
