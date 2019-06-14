296daca01815617f4dffb1bd51e6db8ee56dce8b
Keith Yoon <uniyoona@naver.com>
2019-06-14 13:49:36 +1200

    add locations table/column and seed

---
+++ seeds/locations.js
@@ -0,0 +1,27 @@
+exports.seed = function (knex, Promise) {
+  return knex('locations').del()
+    .then(function () {
+      return knex('locations').insert([
+        {
+          id: 501, locations: 'Under the bed'
+
+        },
+        {
+          id: 502, locations: 'Global'
+
+        },
+        {
+          id: 503, locations: 'In Ketes pants'
+
+        },
+        {
+          id: 504, locations: 'Australia'
+
+        },
+        {
+          id: 505, locations: 'Antartica'
+
+        }
+      ])
+    })
+}
